3696ce847bdb2e9a127fe51b6a241f92f7a3e483
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-11-28 18:18:34 +0100

    Create TodoList.js

---
+++ src/TodoList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ["Learn", "Read", "Write"]
+    };
+  }
+  render() {
+    debugger;
+    let todos = this.state.todos.map((task, index) => (
+      <Todo task={task} key={index} />
+    ));
+    return (
+      <div>
+        <ul>
+          {todos}
+        </ul>
+      </div>
+    );
+  }
+}
