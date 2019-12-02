24c7a8847d282d0f46d10a71b5772a5f4fd79a8c
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-12-02 18:08:50 +0100

    Connect with backend through redux-thunk

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,16 +1,65 @@
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
+export const GET_TODOS = "GET_TODOS";
 
-export function addTodo(task) {
+function handleTodos(data) {
+  debugger;
+  return {
+    type: GET_TODOS,
+    data
+  };
+}
+
+function handleAdd(todo) {
+  debugger;
   return {
     type: ADD_TODO,
-    task
+    todo
+  };
+}
+
+function handleRemove(id) {
+  debugger;
+  return {
+    type: REMOVE_TODO,
+    id
+  };
+}
+
+export function getTodos() {
+  debugger;
+  return dispatch => {          //using redux-thunk - instead of an object, return a function that will execute an asynchronous action
+    return fetch("http://localhost:3001/api/todos")
+      .then(res => res.json())
+      .then(data => dispatch(handleTodos(data)))
+      .catch(err => console.log("SOMETHING WENT WRONG!", err));
+  };
+}
+
+export function addTodo(task) {
+  debugger;
+  return dispatch => {
+    return fetch("http://localhost:3001/api/todos", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({ task })
+    })
+      .then(res => res.json())
+      .then(data => dispatch(handleAdd(data)))
+      .catch(err => console.log("SOMETHING WENT WRONG", err));
   };
 }
 
 export function removeTodo(id) {
-  return {
-    type: REMOVE_TODO,
-    id
+  debugger;
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(data => dispatch(handleRemove(id)))
+      .catch(err => console.log("SOMETHING WENT WRONG", err));
   };
 }
